1f69fab73e1c21cea582111d798425cbda723c87
irmnt <irumikikuchi@gmail.com>
2025-09-15 18:45:22 -0400

    Updated output format

--- weather_checker.py
+++ weather_checker.py
@@ -12,18 +12,46 @@ async def main() -> None:
     
     # Fetch a weather forecast from a city.
     weather = await client.get('Toronto')
+    hour = []
+    hourly_weather = []
+    description = []
     
     # Fetch the temperature for today.
     print(weather.temperature)
     
     # Fetch weather forecast for upcoming days.
     for daily in weather:
-      print(daily)
+      print(daily.date, end=' : ')
     
       # Each daily forecast has their own hourly forecasts.
       for hourly in daily:
-        # print(f' --> {hourly!r}')
-        print(f' --> {hourly.time}, {hourly.temperature}F')
+        # collect data of hour, temperature and sky description
+        hour.append(hourly.time.strftime('%I%p'))
+        hourly_weather.append(hourly.temperature)
+        if len(description) == 0 or description[-1] != hourly.description:
+          description.append(hourly.description) 
+        
+      for i in range(len(description)): 
+        desc = description[i] 
+        print(f'{desc}', end=' ')
+        print('-->', end=' ') if i != len(description)-1 else print(end=' ')
+           
+      #print hour 
+      print(end='\n') 
+      for j in range(len(hour)):
+        print(f' {hour[j]} |', end=' ')
+      
+      #print temperature  
+      print(end='\n')
+      for k in range(len(hourly_weather)):
+        print(f' {hourly_weather[k]}F  |', end=' ')
+        
+      print(end='\n')
+      hour.clear()
+      hourly_weather.clear()
+      description.clear()
+      print('\n')
+
 
 if __name__ == '__main__':
   
